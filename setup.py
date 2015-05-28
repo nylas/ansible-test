932b47ce9cbb42c4dab06e2e11f0e35371477535
Robert McQueen <rob@systemizer.me>
2015-05-28 13:58:33 -0700

    Adding static resources as package data. fixing bugs in ansible-test executable

--- setup.py
+++ setup.py
@@ -7,6 +7,13 @@ setup(
     name='ansible_test',
     version='1.0',
     include_package_data = True,
+    packages = ['ansible_test'],
+    package_data = {
+        "ansible_test": [
+            "resources/Dockerfile.j2",
+            "resources/inventory.yml",
+            "resources/playbook.yml"]
+    },
     scripts=['bin/ansible-test'],
     install_requires=['Jinja2']
 )
